ff3b128e53e6d6a35d3fc3f22fe436dbb90f2467
Ignatius Baskar <PREMKUMARAN.BASKAR@Chubb.com>
2020-07-02 16:01:44 +0530

    second commit

--- logic.js
+++ logic.js
@@ -1,96 +1,118 @@
-
-
-window.addEventListener('load', function () {
-
-    document.getElementById("btn-start-timer").onclick = function () {
-
-        document.getElementById("btn-start-timer").style.display= 'none';
-
-        var team1 = new Promise(function (resolve) {
-            let ultimateTotal= 0;
-            var tableRef = document.getElementById('tableTeam1').getElementsByTagName('tbody')[0];
-            var i = 0;
-            document.getElementById("btn-team-1").onclick= function () {
-                    var tableRow = tableRef.getElementsByTagName('tr')[i];
-                    var total = 0;
-                    for (var j = 1; j < 7; j++) {
-                        var tableCell = tableRow.getElementsByTagName('td')[j];
-                        var ball= Math.floor(Math.random()* 7)
-                        total= total + ball;
-                        var score = document.createTextNode(ball.toString());
-                        tableCell.appendChild(score); 
-                        if (ball == 0) {
-                            break
-                        }       
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+var row = 0;
+var col = 1;
+var total = 0;
+var ultimateTotal = 0;
+function teams(tableId, i, j) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            return [2 /*return*/, new Promise(function (res, rej) {
+                    var change = function () {
+                        ultimateTotal += total;
+                        tableRef.rows[i].cells[7].innerHTML = total.toString();
+                        row++;
+                        col = 0;
+                        total = 0;
+                    };
+                    var tableRef = document.getElementById(tableId).getElementsByTagName('tbody')[0];
+                    var ball = Math.floor(Math.random() * 7);
+                    total += ball;
+                    tableRef.rows[i].cells[j].innerHTML = ball.toString();
+                    if (ball == 0)
+                        change();
+                    if (col == 6)
+                        change();
+                    col++;
+                    console.log(ultimateTotal);
+                    if (row == 10) {
+                        res(ultimateTotal);
                     }
-                    var tableCell = tableRow.getElementsByTagName('td')[7];
-                    var playerTotal = document.createTextNode(total.toString());
-                    tableCell.appendChild(playerTotal);
-                    ultimateTotal= ultimateTotal + total;
-                    document.getElementById("h2-team-1-score").innerHTML = ultimateTotal
-                    i++;
-                if (i == 10){
-                    resolve(ultimateTotal);
-                }
+                })];
+        });
+    });
+}
+var timer = function () { return __awaiter(_this, void 0, void 0, function () {
+    var seconds, team1Score, timerInterval;
+    var _this = this;
+    return __generator(this, function (_a) {
+        document.getElementById('btn-team-1').style.display = 'block';
+        seconds = 60;
+        timerInterval = setInterval(function () {
+            document.getElementById('timer').innerHTML = (--seconds).toString();
+            if (seconds == 0) {
+                clearInterval(timerInterval);
+                document.getElementById('btn-team-1').style.display = 'none';
+                document.getElementById('btn-team-2').style.display = 'none';
+                document.getElementById('h2-winner').innerHTML = "Match Timeout!";
             }
-
-        });
-
-        var team2 = new Promise(function (resolve) {
-            let ultimateTotal= 0;
-            var tableRef = document.getElementById('tableTeam2').getElementsByTagName('tbody')[0];
-            var i= 0;
-            document.getElementById("btn-team-2").onclick = function () {
-                var tableRow = tableRef.getElementsByTagName('tr')[i];
-                var total = 0;
-                for(var j = 1; j < 7; j++ ){
-                    var tableCell = tableRow.getElementsByTagName('td')[j];
-                    var ball= Math.floor(Math.random()* 7);
-                    total = total + ball;
-                    var score = document.createTextNode(ball.toString());
-                    tableCell.appendChild(score);       
-                    if(ball == 0){
-                        break;
-                    }           
+        }, 1000);
+        document.getElementById('btn-team-2').addEventListener('click', function () { return __awaiter(_this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, teams('tableTeam2', row, col)];
+                    case 1:
+                        _a.sent();
+                        document.getElementById('h2-team-2-score').innerHTML = ultimateTotal.toString();
+                        document.getElementById('btn-team-2').style.display = 'none';
+                        clearInterval(timerInterval);
+                        document.getElementById('h2-winner').innerHTML = "Winner: " + ((team1Score > ultimateTotal) ? "Team1" : "Team2") + " by " + ((team1Score > ultimateTotal) ? (team1Score - ultimateTotal) : (ultimateTotal - team1Score));
+                        return [2 /*return*/];
                 }
-                var tableCell = tableRow.getElementsByTagName('td')[7];
-                var playerTotal = document.createTextNode(total.toString());
-                tableCell.appendChild(playerTotal);
-                ultimateTotal = ultimateTotal + total;
-                document.getElementById("h2-team-2-score").innerHTML = ultimateTotal
-                i++;
-                if (i == 10){
-                    resolve(ultimateTotal)
+            });
+        }); });
+        document.getElementById('btn-team-1').addEventListener('click', function () { return __awaiter(_this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, teams('tableTeam1', row, col)];
+                    case 1:
+                        _a.sent();
+                        team1Score = ultimateTotal;
+                        document.getElementById('h2-team-1-score').innerHTML = team1Score;
+                        document.getElementById('btn-team-2').style.display = 'block';
+                        document.getElementById('btn-team-1').style.display = 'none';
+                        row = 0;
+                        col = 1;
+                        ultimateTotal = 0;
+                        return [2 /*return*/];
                 }
-            }
-
-        });
-        
-        var timer = new Promise(function (resolve) {
-            var times = null
-            setTimeout(function () {
-                times= document.getElementById("stats").getElementsByTagName("h1")[1]
-            }, 0);
-            var sec = 60;
-            var timeInterval= setInterval(function () {
-                times.innerHTML = sec;
-                sec--;
-                if(sec == 0){
-                    clearInterval(timeInterval);
-                    resolve("Time Exhausted");
-                }
-            }, 1000);
-            
-        });
-
-        Promise.all([team1, team2]).then(function (res) {
-            document.getElementById("h2-winner").innerHTML = `Winner: ${(res[0]>res[1])?"team1":"team2"} by ${(res[0]>res[1])?res[0]-res[1]:res[1]-res[0]} runs`;
-        });
-
-        timer.then( function (res) {
-            document.getElementById("btn-team-1").style.display = 'none';
-            document.getElementById("btn-team-2").style.display = 'none';
-        });
-
-    };
-  })
+            });
+        }); });
+        return [2 /*return*/];
+    });
+}); };
---
+++ logic.ts
@@ -0,0 +1,66 @@
+let row= 0
+let col= 1
+let total= 0
+let ultimateTotal= 0
+
+async function teams(tableId, i, j){
+    return new Promise(function (res, rej) {
+        let change= ()=>{
+            ultimateTotal+= total 
+            tableRef.rows[i].cells[7].innerHTML= total.toString()   
+            row++
+            col= 0
+            total= 0
+        }
+        var tableRef = document.getElementById(tableId).getElementsByTagName('tbody')[0];
+        var ball= Math.floor(Math.random()* 7)
+        total+= ball
+
+        tableRef.rows[i].cells[j].innerHTML= ball.toString();
+        if(ball==0)
+            change();    
+        if(col==6)
+            change();
+             
+        col++
+        console.log(ultimateTotal)
+        if(row == 10){
+            res(ultimateTotal)           
+        }
+
+    });
+}
+
+let timer= async()=>{
+    document.getElementById('btn-team-1').style.display= 'block';
+    let seconds: number= 60;
+    let team1Score;
+    let timerInterval= setInterval(()=>{
+        document.getElementById('timer').innerHTML= (--seconds).toString();
+        if(seconds == 0) {
+            clearInterval(timerInterval)
+            document.getElementById('btn-team-1').style.display= 'none';
+            document.getElementById('btn-team-2').style.display= 'none';
+            document.getElementById('h2-winner').innerHTML= "Match Timeout!"
+        }
+
+    }, 1000)
+    document.getElementById('btn-team-2').addEventListener('click', async()=>{
+        await teams('tableTeam2', row, col)
+        document.getElementById('h2-team-2-score').innerHTML= ultimateTotal.toString()
+        document.getElementById('btn-team-2').style.display= 'none';
+        clearInterval(timerInterval);
+        document.getElementById('h2-winner').innerHTML= `Winner: ${(team1Score > ultimateTotal)?"Team1":"Team2"} by ${(team1Score > ultimateTotal)?(team1Score-ultimateTotal):(ultimateTotal-team1Score)}`
+    });
+    document.getElementById('btn-team-1').addEventListener('click', async()=>{
+        await teams('tableTeam1', row, col)
+        team1Score= ultimateTotal;
+        document.getElementById('h2-team-1-score').innerHTML= team1Score
+        document.getElementById('btn-team-2').style.display= 'block';
+        document.getElementById('btn-team-1').style.display= 'none';
+        row= 0
+        col= 1
+        ultimateTotal= 0
+    });
+}
+
